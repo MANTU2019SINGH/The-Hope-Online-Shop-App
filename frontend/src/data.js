0be1afbbe071a832a93dac6b8636a626ca2d0cee
Mantu Kumar Singh <mantu93sun@gmail.com>
2022-04-01 02:09:48 +0530

    Frontend file added

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: 'U.S POLO Men T-Shirt',
+      category: 'T-Shirts',
+      image: '/images/uspolo7.PNG',
+      price: 349,
+      brand: 'U.S Polo',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: '2',
+      name: 'Adidas Fit T-Shirt',
+      category: 'T-Shirts',
+      image: '/images/uspolo2.PNG',
+      price: 449,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: '3',
+      name: 'Lacoste Free Shirt',
+      category: 'T-Shirts',
+      image: '/images/uspolo3.PNG',
+      price: 339,
+      brand: 'Lacoste',
+      rating: 4.8,
+      numReviews: 17,
+      description: 'high quality product'
+    },
+    {
+      _id: '4',
+      name: 'U.S POLO Men T-Shirt',
+      category: 'T-Shirts',
+      image: '/images/uspolo4.PNG',
+      price: 649,
+      brand: 'U.S polo',
+      rating: 4.5,
+      numReviews: 14,
+      description: 'high quality product'
+    },
+    {
+      _id: '5',
+      name: 'Puma T-shirt',
+      category: 'T-Shirts',
+      image: '/images/uspolo5.PNG',
+      price: 489,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product'
+    },
+    {
+      _id: '6',
+      name: 'U.S Polo',
+      category: 'T-Shirts',
+      image: '/images/uspolo6.PNG',
+      price: 239,
+      brand: 'U.S polo',
+      rating: 4.5,
+      numReviews: 15,
+      description: 'high quality product'
+    }
+  ]
+}
+export default data;
